b1dca48c5194e22e874245f4d92a653fcc5e900e
Felice Forby <feliceforby@gmail.com>
2018-07-18 21:40:50 +0000

    automatically backed up by learn

--- db/migrate/20180718213612_create_dogs.rb
+++ db/migrate/20180718213612_create_dogs.rb
@@ -1,12 +1,20 @@
 class CreateDogs < ActiveRecord::Migration
-  def up
+  def change
     create_table :dogs do |t|
       t.string :name
       t.string :breed
     end
   end
 
-  def down
-    drop_table :dogs
-  end
+  # Using the up and down methods:
+  # def up
+  #   create_table :dogs do |t|
+  #     t.string :name
+  #     t.string :breed
+  #   end
+  # end
+  # 
+  # def down
+  #   drop_table :dogs
+  # end
 end
